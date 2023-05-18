1abb16ed7cd09793292c693471b27398057ebdd1
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-18 20:53:35 +0600

    Adding Navigation Bar

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,12 +1,55 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
+  return (
+    <div className=" bg-gray-400 text-white pt-14 px-14 ">
+      <div className="grid justify-items-center lg:grid-cols-5 sm:grid-cols-1 gap-6 pb-5 bg-gray-400 text-white">
         <div>
-            <h1 className="text-4xl font-bold underline text-center">Footer</h1>
+          <h1 className=" text-white text-2xl font-bold">Toy's Universe</h1>
+          <p className="my-5">
+          If you're looking for the best pizza in town, look no further than our restaurant!
+          </p>
+          <img src="https://i.ibb.co/hWrj59T/Group-9969.png" alt="" />
         </div>
-    );
+        <div>
+          <h1 className=" text-white text-2xl font-semibold">About Us</h1>
+          <p>About Food</p>
+          <p>About Chef</p>
+          <p>Latest Blogs</p>
+          <p>Best Selling Food</p>
+        </div>
+        <div>
+          <h1 className=" text-white text-2xl font-semibold">Cuisines</h1>
+          <p>Italian</p>
+          <p>American</p>
+          <p>Mexican</p>
+          <p>Chinese</p>
+        </div>
+        <div>
+          <h1 className=" text-white text-2xl font-semibold">Support</h1>
+          <p>Help Desk</p>
+          <p>Sales</p>
+          <p>Become a Partner</p>
+          <p>Developers</p>
+        </div>
+        <div>
+          <h1 className=" text-white text-2xl font-semibold">Address</h1>
+          <p>524 Broadway , NYC</p>
+          <p>+1 777 - 978 - 5570</p>
+        </div>
+          </div>
+          
+          <hr className="h-1" />
+
+          <div className="flex justify-between py-5">
+              <p> © 2023 Toy's Universe. All Rights Reserved</p>
+              <p>Powered by Toy's Universe</p>
+          </div>
+
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -1,12 +1,120 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+// import { AuthContext } from "../../provider/AuthProvider";
 
 const NavBar = () => {
-    return (
-        <div>
-            <h1 className="text-4xl font-bold underline text-center">Navbar</h1>
+//   const { user, logOut } = useContext(AuthContext);
+  // console.log(user);
+
+//   const handleLogout = () => {
+//     logOut()
+//       .then()
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   };
+
+  return (
+    <div>
+      <div className="px-14 py-5 bg-slate-100">
+        <div className="navbar flex">
+          <div className="navbar-start">
+            <Link to="/" className="text-3xl font-bold">
+              Toy's Universe
+            </Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? " text-blue-600 font-bold " : ""
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) =>
+                    isActive ? " text-blue-600 font-bold " : ""
+                  }
+                >
+                  Login
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/register"
+                  className={({ isActive }) =>
+                    isActive ? " text-blue-600 font-bold " : ""
+                  }
+                >
+                  Register
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/chefs"
+                  className={({ isActive }) =>
+                    isActive ? " text-blue-600 font-bold " : ""
+                  }
+                >
+                  Chefs
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/blogs"
+                  className={({ isActive }) =>
+                    isActive ? " text-blue-600 font-bold " : ""
+                  }
+                >
+                  Blogs
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+
+          <div className="navbar-end">
+
+            {/* Logged User Info */}
+            {/* {user?.email ? (
+              <div className="flex items-center">
+                <button
+                  className="btn btn-primary border-none"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+                <img
+                  className="rounded-full w-14 ml-5"
+                  src={user?.photoURL}
+                  alt={user.email}
+                  title={user.displayName}
+                />
+              </div>
+            ) : (
+              <Link to="/login">
+                <li className="btn btn-primary border-none">Login</li>
+              </Link>
+            )} */}
+                      
+                      <li className="btn btn-primary border-none">Login</li>
+
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
