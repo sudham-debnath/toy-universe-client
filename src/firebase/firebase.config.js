d3b59f598e378e51b582adf4e625790a7e4384c5
SudhamDebnath <sudham15-12854@diu.edu.bd>
2023-05-18 18:44:38 +0600

    Hide Firebase Environment Variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+// console.log('environment variable', import.meta.env.VITE_apiKey)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA-Bhh9YX8qeFeBfjt6zAv7f9JxuiuVm8Y",
-  authDomain: "toy-universe-client.firebaseapp.com",
-  projectId: "toy-universe-client",
-  storageBucket: "toy-universe-client.appspot.com",
-  messagingSenderId: "309396918984",
-  appId: "1:309396918984:web:1eb0cf95b94f8169d68bfd"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
